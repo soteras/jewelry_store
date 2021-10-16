9bce56320abd3adf9fc85dfcd2aa660f4c574da2
Soteras <carlosoteras@gmail.com>
2021-10-16 06:45:46 -0300

    Configure swagger.

--- lib/jewelry_store_web/router.ex
+++ lib/jewelry_store_web/router.ex
@@ -5,12 +5,28 @@ defmodule JewelryStoreWeb.Router do
     plug :accepts, ["html"]
   end
 
+  pipeline :api_swagger do
+    plug OpenApiSpex.Plug.PutApiSpec, module: JupiterWeb.Swagger.ApiSpec
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   pipeline :auth do
     plug JewelryStore.Authentication.Pipeline
+  end
+
+  scope "/v1" do
+    pipe_through :api_swagger
+
+    get "/openapi", OpenApiSpex.Plug.RenderSpec, []
+  end
+
+  scope "/" do
+    pipe_through :browser
+
+    get "/swaggerui", OpenApiSpex.Plug.SwaggerUI, path: "/v1/openapi"
   end
 
   scope "/public/v1", JewelryStoreWeb do
