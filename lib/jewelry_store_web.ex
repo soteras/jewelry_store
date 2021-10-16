f70c99666f6bf16f6de2f5f9354621dc6b339b26
Soteras <carlosoteras@gmail.com>
2021-10-16 06:56:53 -0300

    Handle responses methods.

--- lib/jewelry_store_web.ex
+++ lib/jewelry_store_web.ex
@@ -1,29 +1,19 @@
 defmodule JewelryStoreWeb do
-  @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, views, channels and so on.
+  alias JewelryStoreWeb.JewelryStoreView
 
-  This can be used in your application as:
-
-      use JewelryStoreWeb, :controller
-      use JewelryStoreWeb, :view
-
-  The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define any helper function in modules
-  and import those modules here.
-  """
+  import Phoenix.Controller, only: [put_view: 2, render: 3]
+  import Plug.Conn
 
   def controller do
     quote do
       use Phoenix.Controller, namespace: JewelryStoreWeb
 
+      alias JewelryStoreWeb.Router.Helpers, as: Routes
+
       import Plug.Conn
       import JewelryStoreWeb.Gettext
-      alias JewelryStoreWeb.Router.Helpers, as: Routes
+      import JewelryStoreWeb, only: [render_response: 2, render_response: 3]
+      import Guardian.Plug, only: [current_resource: 1, current_resource: 2]
     end
   end
 
@@ -67,6 +57,33 @@ defmodule JewelryStoreWeb do
       import JewelryStoreWeb.Gettext
       alias JewelryStoreWeb.Router.Helpers, as: Routes
     end
+  end
+
+  @spec render_response(nil, Plug.Conn.t()) :: nil
+  def render_response(nil, _conn), do: nil
+
+  @spec render_response(map, Plug.Conn.t()) :: Plug.Conn.t()
+  def render_response(%{data: data, pagination: pagination}, conn) do
+    conn
+    |> put_status(:ok)
+    |> put_view(JewelryStoreView)
+    |> render("paginated.json", %{data: data, pagination: pagination})
+  end
+
+  @spec render_response(atom, Plug.Conn.t()) :: Plug.Conn.t()
+  def render_response(:empty, conn) do
+    conn
+    |> put_status(:ok)
+    |> put_view(JewelryStoreView)
+    |> render("response.json", data: :empty)
+  end
+
+  @spec render_response(map | list, Plug.Conn.t()) :: Plug.Conn.t()
+  def render_response(data, conn) do
+    conn
+    |> put_status(:ok)
+    |> put_view(JewelryStoreView)
+    |> render("response.json", data: data)
   end
 
   @doc """
