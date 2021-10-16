41ab41fa07637bbd77fc1dfdb2f5560ee854680f
Soteras <carlosoteras@gmail.com>
2021-10-16 06:48:32 -0300

    Add fallback controller.

---
+++ lib/jewelry_store_web/controllers/fallback_controller.ex
@@ -0,0 +1,26 @@
+defmodule JewelryStore.FallbackController do
+  use Phoenix.Controller
+
+  alias JewelryStoreWeb.ErrorView
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(ErrorView)
+    |> render("error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(ErrorView)
+    |> render(:"404")
+  end
+
+  def call(conn, {:error, :unauthorized}) do
+    conn
+    |> put_status(403)
+    |> put_view(ErrorView)
+    |> render(:"403")
+  end
+end
