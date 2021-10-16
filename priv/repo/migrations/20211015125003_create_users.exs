84e3c7d3ed9b6634ac22887e692ebab8fc2ea60e
Soteras <carlosoteras@gmail.com>
2021-10-15 23:17:42 -0300

    Add user model.

---
+++ priv/repo/migrations/20211015125003_create_users.exs
@@ -0,0 +1,22 @@
+defmodule JewelryStore.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string, null: false
+      add :cpf, :string, null: false, size: 11
+      add :rg, :string
+      add :gender, :string, null: false, default: "female"
+      add :nickname, :string
+      add :birthdate, :date
+      add :cellphone, :string, null: false
+      add :email, :string, null: false
+      add :password_hash, :string, null: false
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email])
+    create unique_index(:users, [:cpf])
+  end
+end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -4,13 +4,13 @@ defmodule JewelryStore.Factory do
   def user_factory do
     %JewelryStore.Users.User{
       name: "Maria da Silva",
-      cpf: "09986414059",
+      cpf: "92785663063",
       rg: "273394721",
       gender: "female",
       nickname: "Mari",
       birthdate: ~D[2021-10-15],
       cellphone: "(11) 98999-9999",
-      email: "maria@gmail.com",
+      email: "mari@gmail.com",
       password_hash: Bcrypt.hash_pwd_salt("abc12345")
     }
   end
