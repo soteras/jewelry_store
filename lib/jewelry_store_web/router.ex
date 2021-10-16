fab9bbd2d3ae6f22f56ba8e0c4a61703517c4133
Soteras <carlosoteras@gmail.com>
2021-10-16 00:40:22 -0300

    Configure auth in routes.

--- lib/jewelry_store_web/router.ex
+++ lib/jewelry_store_web/router.ex
@@ -1,11 +1,23 @@
 defmodule JewelryStoreWeb.Router do
   use JewelryStoreWeb, :router
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/api", JewelryStoreWeb do
+  pipeline :auth do
+    plug JewelryStore.Authentication.Pipeline
+  end
+
+  scope "/public/v1", JewelryStoreWeb do
     pipe_through :api
+  end
+
+  scope "/v1", JewelryStoreWeb do
+    pipe_through [:auth, :api]
   end
 end
