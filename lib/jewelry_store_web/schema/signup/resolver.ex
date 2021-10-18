c27cace96c089acc7bcc17ff751e4b255a42b9b5
Soteras <carlosoteras@gmail.com>
2021-10-18 00:01:17 -0300

    Add resolver.

--- lib/jewelry_store_web/schema/signup/resolver.ex
+++ lib/jewelry_store_web/schema/signup/resolver.ex
@@ -1,7 +1,12 @@
 defmodule JewelryStoreWeb.Schema.Signup.Resolver do
-  def create_post(_parent, args, _context) do
-    IO.inspect(args)
+  alias JewelryStore.Authentication
+  alias JewelryStore.Users
 
-    {:ok, %{name: "Carlos", nickname: "Soteras", token: "abc123"}}
+  def create_post(_parent, args, _context) do
+    with input <- Map.get(args, :input),
+         {:ok, %{id: id, name: name, nickname: nickname}} <- Users.create_user(input),
+         {:ok, token} <- Authentication.generate_refresh_token(%{id: id}) do
+      {:ok, %{token: token, name: name, nickname: nickname}}
+    end
   end
 end
