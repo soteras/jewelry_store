49c5fabdc6828453db5d56d0e65d5970f41d6f5e
Soteras <carlosoteras@gmail.com>
2021-10-17 23:12:48 -0300

    Add signup types.

--- lib/jewelry_store_web/schema/signup/type.ex
+++ lib/jewelry_store_web/schema/signup/type.ex
@@ -1,13 +1,34 @@
 defmodule JewelryStoreWeb.Schema.Signup.Type do
   use Absinthe.Schema.Notation
 
+  import_types Absinthe.Type.Custom
+
   object :session do
     field :name, :string
     field :nickname, :string
     field :token, :string
   end
 
+  enum :gender do
+    description "Tipos de gêneros"
+
+    value :female, description: "Feminino"
+    value :male, description: "Masculino"
+    value :other, description: "Outro"
+  end
+
   input_object :signup_input do
-    field :name, non_null(:string)
+    description "Campos da criação do usuário"
+
+    field :name, non_null(:string), description: "Nome"
+    field :cpf, non_null(:string), description: "CPF"
+    field :gender, non_null(:gender), description: "Gênero"
+    field :cellphone, non_null(:string), description: "Número de celular"
+    field :email, non_null(:string), description: "E-mail"
+    field :password, non_null(:string), description: "Senha"
+    field :password_confirmation, non_null(:string), description: "Confirmação da senha"
+    field :rg, :string, description: "RG"
+    field :nickname, :string, description: "Apelido"
+    field :birthdate, :date, description: "Data de aniversário"
   end
 end
