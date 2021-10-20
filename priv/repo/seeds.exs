96dbd8be2671fedb6ccc397ca2e177bbc0873c94
Soteras <carlosoteras@gmail.com>
2021-10-20 19:24:08 -0300

    Add seeds.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,36 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     JewelryStore.Repo.insert!(%JewelryStore.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias JewelryStore.Repo
+alias JewelryStore.Users.User
+alias JewelryStore.Products.Category
+
+%User{}
+|> User.changeset(%{
+  name: "Usuário teste",
+  cpf: "86838272083",
+  gender: "MALE",
+  cellphone: "(11) 98999-9999",
+  email: "test@gmail.com",
+  password: "abc12345",
+  password_confirmation: "abc12345",
+  nickname: "Teste"
+})
+|> Repo.insert!()
+
+%User{
+  name: "Usuário admin",
+  cpf: "39566169019",
+  gender: "MALE",
+  cellphone: "(11) 98999-9999",
+  email: "admin@gmail.com",
+  nickname: "Admin",
+  password_hash: Bcrypt.hash_pwd_salt("abc12345"),
+  admin: true
+}
+|> Repo.insert!()
+
+%Category{}
+|> Category.changeset(%{name: "Brincos Pequenos", active: true})
+|> Repo.insert!()
+
+%Category{}
+|> Category.changeset(%{name: "Conjuntos com Zircônias", active: true})
+|> Repo.insert!()
