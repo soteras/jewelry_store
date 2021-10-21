0e93bed7111c6af02e8116573acb379c5a4c71d7
Soteras <carlosoteras@gmail.com>
2021-10-20 21:38:06 -0300

    query categories.

--- lib/jewelry_store_web/schema/categories/resolver.ex
+++ lib/jewelry_store_web/schema/categories/resolver.ex
@@ -17,4 +17,15 @@ defmodule JewelryStoreWeb.Schema.Categories.Resolver do
       {:ok, %{name: name, active: active, slug: slug}}
     end
   end
+
+  def get_categories(_parent, _args, %{context: %{current_user: current_user}}) do
+    with :ok <- Bodyguard.permit(Products, :list, current_user),
+         {:ok, categories} <- Products.list_categories() do
+      {:ok, Enum.map(categories, &build_category/1)}
+    end
+  end
+
+  defp build_category(category) do
+    %{name: category.name, active: category.active, slug: category.slug}
+  end
 end
