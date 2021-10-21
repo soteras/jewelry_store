8addd08babec979c158860b3aa97b81e942b06f8
Soteras <carlosoteras@gmail.com>
2021-10-20 22:12:35 -0300

    Change desc.

--- lib/jewelry_store_web/schema/categories/input.ex
+++ lib/jewelry_store_web/schema/categories/input.ex
@@ -2,7 +2,7 @@ defmodule JewelryStoreWeb.Schema.Categories.Input do
   use Absinthe.Schema.Notation
 
   input_object :category_input do
-    description("Campos da criação de categoria")
+    description("Input de categoria")
 
     field :name, non_null(:string), description: "Nome"
     field :active, non_null(:boolean), description: "Ativo?"
