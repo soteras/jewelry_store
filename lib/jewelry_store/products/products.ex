b1c6b5127e9162e20c0f5a340d72bee94cedb328
Soteras <carlosoteras@gmail.com>
2021-10-22 23:46:43 -0300

    Create product.

--- lib/jewelry_store/products/products.ex
+++ lib/jewelry_store/products/products.ex
@@ -1,9 +1,12 @@
 defmodule JewelryStore.Products do
   alias JewelryStore.Products.DbCategory
+  alias JewelryStore.Products.DbProduct
 
   defdelegate authorize(action, user, params), to: JewelryStore.Authorization.Policies.Admin
   defdelegate create_category(attrs), to: DbCategory
   defdelegate update_category(category, attrs), to: DbCategory
   defdelegate get_category_by_id(id), to: DbCategory
   defdelegate list_categories(), to: DbCategory
+
+  defdelegate create_product(attrs), to: DbProduct
 end
